cfde7a5f4578d3caf223ab4ba60af86af60e7d75
Phil Monroe <phil@philmonroe.com>
2015-09-17 16:46:47 -0700

    start on readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Lale.Help
+
+This is the rails application used for lale.help.
+
+## System Setup
+
+To setup your system on OS X, just run:
+
+    ./bin/setup-osx.sh
+
---
+++ bin/setup-osx.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+echo "== Updating Homebrew =="
+brew update
+
+
+echo "== Installing PostgreSQL =="
+brew install postgresql
+brew upgrade postgresql || true
+
+
+echo "== Installing ruby =="
+brew install rbenv
+brew install ruby-build
+rbenv install -s 2.2.3
+
+if [ -z "$RBENV_SHELL" ]; then
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  eval "$(rbenv init -)"
+fi
+
+
+echo "== Installing Bundler =="
+gem install bundler
+rbenv rehash
+
+
+echo "== Installing Gems =="
+bundle install -j8 --path .bundle
+
+
+echo "== Setup DB =="
+bin/rake db:init
+bin/rake postgresql:start &
+bin/rake db:create db:migrate
+
+PG_PID=$(cat .postgres/postmaster.pid | head -n 1)
+kill $PG_PID
+wait $PG_PID
+
+
+echo "== Finished! =="
