1536f39cdfb71de9de0052cc5501e52e66d10e9e
James Fan <james.fan@workday.com>
2015-09-24 15:10:22 -0700

    Add basic form styles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/ping', to: 'website#ping'
   get '/stylesheet', to: 'website#stylesheet'
+  get '/signup', to: 'website#signup'
   root to: 'website#index'
 end
