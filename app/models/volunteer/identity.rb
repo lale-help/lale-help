eea5dd64403b3e0d8c5d474f0f53ca74de577432
Phil Monroe <phil@philmonroe.com>
2015-09-24 16:19:33 -0700

    get user registration working

--- app/models/volunteer/identity.rb
+++ app/models/volunteer/identity.rb
@@ -5,5 +5,14 @@ class Volunteer::Identity < OmniAuth::Identity::Models::ActiveRecord
               presence: true,
               uniqueness: true,
               format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :first_name, :last_name, :location, presence: true
+
+
+  before_create do
+    Authentication::VolunteerRegistration.run!(identity: self)
+  end
+
+  attr_accessor :first_name, :last_name, :location
+
 
 end
