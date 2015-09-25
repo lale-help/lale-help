ac46b2efd5f982307fe289fea2c031edf64265ca
Phil Monroe <phil@philmonroe.com>
2015-09-25 00:05:44 -0700

    merging master

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,16 +1,25 @@
 class Circle < ActiveRecord::Base
+  attr_accessor :location_text
+
   has_many :working_groups
   has_many :volunteers
-
   has_many :tasks, through: :working_groups
 
   belongs_to :location
   belongs_to :admin, class_name: 'Volunteer'
 
   validates :name, presence: true
-  attr_accessor :location_text
+  validates :location_text, presence: true
 
   before_save :determine_location
+
+  def volunteer_count
+    volunteers.count
+  end
+
+  def open_task_count
+    tasks.count
+  end
 
   private
   def determine_location
--- app/views/circles/index.json.jbuilder
+++ app/views/circles/index.json.jbuilder
@@ -2,7 +2,7 @@ json.center @center
 
 json.circles do
   json.array!(@circles) do |circle|
-    json.(circle, :id, :name)
+    json.(circle, :id, :name, :volunteer_count, :open_task_count)
     json.location circle.location
   end
 end
