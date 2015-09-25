1872105dacc7e2e5388d265343de7df1dcb0bff8
andrew.bullen <andrew.bullen@workday.com>
2015-09-24 18:32:53 -0700

    initial styling for edit task page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,11 +30,9 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to [@circle, @task], notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
+        format.html { redirect_to circle_tasks_path(@circle), notice: 'Task was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -44,11 +42,9 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
+        format.html { redirect_to circle_tasks_path(@circle), notice: 'Task was successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -58,7 +54,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to circle_tasks_url(@circle), notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
