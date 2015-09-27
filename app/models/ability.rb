40975412250f783d78cdb6b4d64abe4205b250af
Tejas R Mandke <tejas.mandke@gmail.com>
2015-09-27 00:25:25 -0700

    update ability to be more accurate

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,17 +7,19 @@ class Ability
     # Circles
     can :read, Circle
     can :create, Circle unless user.circle.present?
-    can [:update, :destroy], Circle, admin: user
+    if user.circle
+      can [:update, :destroy], Circle, admin: user
 
-    # Work Groups
-    can :read, WorkingGroup, circle: user.circle
-    can [:create, :update, :destroy], WorkingGroup, circle: { admin: user }
+      # Work Groups
+      can :read, WorkingGroup, circle_id: user.circle_id
+      can [:create, :update, :destroy], WorkingGroup, circle: { admin: user }
 
-    # Tasks
-    can :read, Task, working_group: { circle: user.circle }
-    can [:create, :update, :destroy], Task, working_group: { circle: { admin: user } }
-    can :volunteer, Task, working_group: { circle: user.circle }
-    cannot :volunteer, Task, volunteer_assignments: { volunteer: user }
-    can :complete, Task, volunteer_assignments: { volunteer: user }
+      # Tasks
+      can :read, Task, working_group: { circle_id: user.circle_id }
+      can [:create, :update, :destroy], Task, working_group: { circle: { admin: user } }
+      can :volunteer, Task, working_group: { circle: user.circle }
+      cannot :volunteer, Task, volunteer_assignments: { volunteer: user }
+      can :complete, Task, volunteer_assignments: { volunteer: user }
+    end
   end
 end
