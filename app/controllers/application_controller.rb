fa032eea493360eaca7b8b92877dea90e4efcdcf
Tejas R Mandke <tejas.mandke@gmail.com>
2015-09-30 20:26:08 -0700

    enforce authentication and add some error display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  check_authorization
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
 
   helper_method def current_user
     @current_user ||= session[:user_id] && Volunteer.find(session[:user_id])
