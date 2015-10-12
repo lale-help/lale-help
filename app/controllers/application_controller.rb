9aaacb3693954e0a55d067de730a7ca7a75f1f80
Phil Monroe <phil@philmonroe.com>
2015-10-11 19:23:32 -0700

    internationalize the remaining circle templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,6 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
-    puts I18n.locale
   end
   before_action :set_locale
 end
