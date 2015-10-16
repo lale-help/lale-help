ef4247cb88861d1e57890871d41c085d72f5d523
Michael Dauz <michaeldauz@gmail.com>
2015-10-16 12:46:09 -0700

    change how date is displayed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def humanize_date(date)
     # distance_of_time_in_words Time.now, date
-    date.strftime("%d %B %Y")
+    date.strftime("%d %b %Y")
   end
 
   def classes_for_task task
