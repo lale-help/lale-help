a411c3354796baa778fec8c47cdc05675e16b252
Phil Monroe <phil@philmonroe.com>
2015-10-27 09:43:25 -0700

    setting up mandrill

---
+++ app/mailers/base_mandrill_mailer.rb
@@ -0,0 +1,26 @@
+require "mandrill"
+
+class BaseMandrillMailer < ActionMailer::Base
+  default(
+    from: "info@lale.help",
+    reply_to: "info@lale.help"
+  )
+
+  private
+
+  def send_mail(email, subject, body)
+    mail(to: email, subject: subject, body: body, content_type: "text/html")
+  end
+
+  def mandrill_template(template_name, locale, attributes)
+    mandrill = Mandrill::API.new(ENV["SMTP_PASSWORD"])
+
+    merge_vars = attributes.map do |key, value|
+      { name: key, content: value }
+    end
+
+    template_name = "#{template_name} - #{locale}"
+
+    mandrill.templates.render(template_name, [], merge_vars)["html"]
+  end
+end
