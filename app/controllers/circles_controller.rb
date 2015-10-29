2fa7f4595ed307d79ca829b3cd11099ab2e58555
Phil Monroe <phil@philmonroe.com>
2015-10-28 17:18:03 -0700

    show current_user's tasks on the dashboard

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -22,6 +22,7 @@ class CirclesController < ApplicationController
   # GET /circles/1
   # GET /circles/1.json
   def show
+    @my_tasks = current_circle.tasks.not_completed.joins(:volunteer_assignments).where(task_volunteer_assignments: { volunteer_id: current_user } )
   end
 
   # GET /circles/new
