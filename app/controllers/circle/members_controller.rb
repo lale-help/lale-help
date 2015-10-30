34726c4e2ee08b9468ac6510f197c9aa46c03540
Phil Monroe <phil@philmonroe.com>
2015-10-30 15:07:39 -0700

    members and admin pages for circle

--- app/controllers/circle/members_controller.rb
+++ app/controllers/circle/members_controller.rb
@@ -6,5 +6,9 @@ class Circle::MembersController < ApplicationController
   include HasWorkingGroupFilters
 
   def index
+    members = current_circle.users.select('users.*, count(distinct circle_roles.*) as circle_role_count').group('users.id').order('circle_role_count DESC')
+    members = members.joins(:working_group_roles).where(working_group_roles: { working_group: current_working_group }) if current_working_group.present?
+
+    @members = members.distinct.includes(:identity, :working_groups, :circle_roles)
   end
 end
