ae072c65d0bf4cd939b2fc4a8e7db02ff2efb9bc
Phil Monroe <phil@philmonroe.com>
2015-10-30 15:21:15 -0700

    only show circle roles for current circle

--- app/models/circle/role.rb
+++ app/models/circle/role.rb
@@ -11,6 +11,7 @@ class Circle::Role < ActiveRecord::Base
   ]
 
   scope :leadership, ->{ where(role_type: %w(circle.admin circle.official circle.custom)) }
+  scope :for_circle, ->(circle) { where(circle: circle ) }
 
   def name
     self[:name] || I18n.t!("roles.name.#{self.role_type}")
