f91b80c2204c792e0076f6b8fbf2b336ed8a5d46
Phil Monroe <phil@philmonroe.com>
2015-11-19 13:01:40 -0800

    i18n supplies show

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -5,10 +5,6 @@ class Supply < ActiveRecord::Base
   # Associations
   belongs_to :location
 
-  def due_date_text
-    due_date.strftime("%A %-d %B %Y")
-  end
-
   def volunteer
     volunteers.first
   end
