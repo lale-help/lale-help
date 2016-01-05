631ad7107aee9d8019c92157bb2118201ade5a1a
Phil Monroe <phil@philmonroe.com>
2016-01-05 10:57:54 -0600

    fallback to english when building emails if a template or subject is missing

--- app/mailers/base_mandrill_mailer.rb
+++ app/mailers/base_mandrill_mailer.rb
@@ -24,14 +24,21 @@ class BaseMandrillMailer < ActionMailer::Base
     I18n.with_locale(lang) do
       merge_vars = block.call
       merge_vars.merge!(defaule_merge_vars)
-      body       = fetch_template(merge_vars)
+
+      body = begin
+        fetch_template(I18n.locale, merge_vars)
+      rescue Mandrill::UnknownTemplateError
+        fetch_template("en", merge_vars)
+      end
+
       send_mail(email, subject, body)
     end
   end
 
   def subject
     tag     = I18n.t('mailers.subject-tag', default: "[Lale]")
-    content = I18n.t!([:mailers, :subject, mailer_name, action_name].join(?.))
+    key     = [:mailers, :subject, mailer_name, action_name].join(?.)
+    content = I18n.t!(key, default: I18n.t!(key, locale: :en))
     "#{tag} #{content}"
   end
 
@@ -45,34 +52,36 @@ class BaseMandrillMailer < ActionMailer::Base
   end
 
 
-  def fetch_template(attributes)
+  def fetch_template(lang, attributes)
+    template = template_name(lang)
+
     merge_vars = attributes.map do |key, value|
       { name: key, content: value }
     end
 
     if Rails.application.config.mandrill_templates
-      _template_from_mandrill(merge_vars)
+      _template_from_mandrill(template, merge_vars)
     else
-      _template_debug(attributes)
+      _template_debug(template, attributes)
     end
   end
 
-  def _template_debug merge_vars
+  def _template_debug template, merge_vars
     {
       locale: locale,
-      template_name: template_name,
+      template_name: template,
       merge_vars: merge_vars
     }.to_yaml
   end
 
-  def _template_from_mandrill(merge_vars)
+  def _template_from_mandrill(template, merge_vars)
     mandrill      = Mandrill::API.new(ENV["SMTP_PASSWORD"])
-    mandrill.templates.render(template_name, [], merge_vars)["html"]
+    mandrill.templates.render(template, [], merge_vars)["html"]
   end
 
-  def template_name
+  def template_name lang
     scope = mailer_name.gsub(/_mailer/, "")
-    "#{I18n.locale}/#{scope}/#{action_name}"
+    "#{lang}/#{scope}/#{action_name}"
   end
 
 end
