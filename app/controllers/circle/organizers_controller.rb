63fcb605be622dd22e264b28547ea458c3124237
Phil Monroe <phil@philmonroe.com>
2016-02-01 23:01:12 -0800

    compute organizers based on circle admins and working group admins

--- app/controllers/circle/organizers_controller.rb
+++ app/controllers/circle/organizers_controller.rb
@@ -5,8 +5,25 @@ class Circle::OrganizersController < ApplicationController
   include HasCircle
 
   def index
-    members = current_circle.users.select('users.*, count(distinct circle_roles.*) as circle_role_count').group('users.id').order('circle_role_count DESC')
+    circle_organizers = User.
+      select('users.id, count(*) as role_count').
+      joins(:circle_roles).
+      where(circle_roles: { role_type: Circle::Role::ORGANIZER_TYPE_IDS, circle: current_circle }).
+      group('users.id')
 
-    @members = members.distinct.includes(:identity, :working_groups, :circle_roles)
+    working_group_organizers = User.
+      select('users.id, count(*) as role_count').
+      joins(:working_group_roles).
+      where(working_group_roles: { role_type: WorkingGroup::Role::ORGANIZER_TYPE_IDS, working_group_id: current_circle.working_groups }).
+      group('users.id')
+
+    counts = (circle_organizers + working_group_organizers).each_with_object(Hash.new) do |user, obj|
+      obj[user.id] ||= 0
+      obj[user.id] += user.role_count
+    end
+
+    all_organizers = User.where(id: counts.keys).includes(:identity, :working_groups, :circle_roles)
+
+    @members = all_organizers.sort_by{|u| -counts[u.id] }
   end
 end
