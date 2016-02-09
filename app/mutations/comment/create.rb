931113f08c701e851ee97034853d7fbea44a6b95
Phil Monroe <phil@philmonroe.com>
2016-02-08 22:26:07 -0800

    Relates #110: Stub out action to send email on supply comment

--- app/mutations/comment/create.rb
+++ app/mutations/comment/create.rb
@@ -1,16 +1,14 @@
 class Comment::Create < Comment::BaseForm
   class Submit < Comment::BaseForm::Submit
+    def mailer_class
+      task.is_a? Task ? TaskMailer : SupplyMailer
+    end
+
     def execute
       super.tap do |outcome|
         (task.users.uniq - [ commenter ]).each do |outboud_user|
-          if task.is_a? Task
-            TaskMailer.task_comment(task, comment, outboud_user).deliver_now
-          else
-            # SupplyMailer.task
-          end
+          mailer_class.task_comment(task, comment, outboud_user).deliver_now
         end
-
-
       end
     end
   end
