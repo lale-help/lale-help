2bca440d104fec70ff627c672eca73f8b14d9257
Phil Monroe <phil@philmonroe.com>
2016-02-08 22:21:14 -0800

    Relates #110: Send email on task comment

--- app/mutations/comment/create.rb
+++ app/mutations/comment/create.rb
@@ -2,7 +2,15 @@ class Comment::Create < Comment::BaseForm
   class Submit < Comment::BaseForm::Submit
     def execute
       super.tap do |outcome|
-        #TODO: send email
+        (task.users.uniq - [ commenter ]).each do |outboud_user|
+          if task.is_a? Task
+            TaskMailer.task_comment(task, comment, outboud_user).deliver_now
+          else
+            # SupplyMailer.task
+          end
+        end
+
+
       end
     end
   end
