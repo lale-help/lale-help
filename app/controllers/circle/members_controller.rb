f340e9fd7ff38f62409f88107a56df2bee564b7f
Phil Monroe <phil@philmonroe.com>
2016-02-14 17:43:59 -0800

    better access control around member lists

--- app/controllers/circle/members_controller.rb
+++ app/controllers/circle/members_controller.rb
@@ -5,11 +5,7 @@ class Circle::MembersController < ApplicationController
   include HasCircle
 
   def index
-    if current_user.admin?
-      members = current_circle.users.select('users.*, count(distinct circle_roles.*) as circle_role_count').group('users.id').order('circle_role_count DESC')
-    else
-      members = current_circle.users.select('users.*, count(distinct circle_roles.*) as circle_role_count').where("public_profile = 'true'").group('users.id').order('circle_role_count DESC')
-    end
+    members = current_circle.users.select('users.*, count(distinct circle_roles.*) as circle_role_count').group('users.id').order('circle_role_count DESC')
     @members = members.distinct.includes(:identity, :working_groups, :circle_roles)
   end
 
