5078a404dbaaa32bb36777da4b2ee41a28a1672f
Phil Monroe <phil@philmonroe.com>
2016-02-14 17:26:40 -0800

    only send comment emails for tasks

--- app/mutations/comment/create.rb
+++ app/mutations/comment/create.rb
@@ -1,13 +1,11 @@
 class Comment::Create < Comment::BaseForm
   class Submit < Comment::BaseForm::Submit
-    def mailer_class
-      task.is_a?(Task) ? TaskMailer : SupplyMailer
-    end
-
     def execute
       super.tap do |outcome|
-        (task.users.uniq - [ commenter ]).each do |outboud_user|
-          mailer_class.task_comment(task, comment, outboud_user).deliver_now
+        if task.is_a? Task
+          (task.users.uniq - [ commenter ]).each do |outboud_user|
+            TaskMailer.task_comment(task, comment, outboud_user).deliver_now
+          end
         end
       end
     end
