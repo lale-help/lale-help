9eb1b8c8474c499a3a524ffa774c93d989fbcd87
Kristy Gateley <kristysnet@gmail.com>
2016-02-20 21:21:39 -0800

    On signup, do circle search based on address fields instead of location

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ActiveRecord::Base
   has_many :users
   belongs_to :location
 
+  def location_query
+    [city, state_province, country].compact!.delete_if{ |s| s.empty? }.join(', ')
+  end
+
 end
