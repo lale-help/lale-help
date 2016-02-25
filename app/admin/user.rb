516893c49c0ae603a708305cc1139149f8d9c95c
Phil Monroe <phil@philmonroe.com>
2016-02-24 21:27:47 -0800

    ability to 'become' a different user for site admin testing purposes

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,17 @@
 ActiveAdmin.register User do
 
+  member_action :become, method: :get do
+    begin
+      user = User.find(params[:id])
+      login(user)
+      redirect_to root_path
+    rescue
+      redirect_to :back, notice: "Unable to become user with id #{params[:id]}"
+    end
+  end
+
+  action_item :become, only: :show do
+    link_to 'Become this User', become_admin_user_path
+  end
 
 end
