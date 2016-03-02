c0c2139a9a3e472434fb48a4d7461ea914470ff5
Phillip Oertel <me@phillipoertel.com>
2016-03-02 14:40:17 +0100

    Removed code that's unused and mostly boilerplate

--- app/controllers/circle/calendars_controller.rb
+++
@@ -1,9 +0,0 @@
-class Circle::CalendarsController < ApplicationController
-  before_action :ensure_logged_in
-  skip_authorization_check # TODO: REMOVE
-
-  include HasCircle
-
-  def show
-  end
-end
