c53d566657929c06b9b2eabe130dca6f003266c2
Kristy Gateley <kristysnet@gmail.com>
2016-03-02 14:24:49 -0800

    Added total member count Separated work group links by comma Changed location column to use new address model Ordered members by last name

--- app/controllers/circle/members_controller.rb
+++ app/controllers/circle/members_controller.rb
@@ -5,8 +5,9 @@ class Circle::MembersController < ApplicationController
   include HasCircle
 
   def index
-    members = current_circle.users.select('users.*, count(distinct circle_roles.*) as circle_role_count').group('users.id').order('circle_role_count DESC')
-    @members = members.distinct.includes(:identity, :working_groups, :circle_roles)
+    members = current_circle.users.select('users.*, count(distinct circle_roles.*) as circle_role_count').group('users.id').order('users.last_name asc, circle_role_count DESC')
+    @members = members.distinct.where('users.public_profile = true').includes(:identity, :working_groups, :circle_roles)
+    @total_members = members.distinct.length
   end
 
   def show
