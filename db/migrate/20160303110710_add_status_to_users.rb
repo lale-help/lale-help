b5bcbd7851b78e9745b3e40b9a5ee001ca9dc89e
Phillip Oertel <me@phillipoertel.com>
2016-03-04 16:11:08 +0100

    Hope to fix migration this time

--- db/migrate/20160303110710_add_status_to_users.rb
+++ db/migrate/20160303110710_add_status_to_users.rb
@@ -1,6 +1,6 @@
 class AddStatusToUsers < ActiveRecord::Migration
   def change
     add_column :users, :status, :integer
-    User.update_all(:status => :active)
+    User.update_all(:status => User.statuses[:active])
   end
 end
