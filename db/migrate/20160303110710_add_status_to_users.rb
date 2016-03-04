35c96215cd5fa7b325013b0965a41ab0d6a4475e
Phillip Oertel <me@phillipoertel.com>
2016-03-04 15:54:49 +0100

    Dude ...

--- db/migrate/20160303110710_add_status_to_users.rb
+++ db/migrate/20160303110710_add_status_to_users.rb
@@ -1,6 +1,6 @@
 class AddStatusToUsers < ActiveRecord::Migration
   def change
-    User.update_all(:status => :active)
     add_column :users, :status, :integer
+    User.update_all(:status => :active)
   end
 end
