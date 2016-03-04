13b315bb508c1c46a3f387a25360c730c5d4901d
Phillip Oertel <me@phillipoertel.com>
2016-03-04 10:38:19 +0100

    Test setup: don't set Accept-Language, Only works for poltergeist, which I disabled temp.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,10 +54,11 @@ RSpec.configure do |config|
     Rails.logger.info "-"*80
   end
 
-  config.before(:each, js: true) do
-    # feature specs need to use lale in English to find labels etc.
-    page.driver.headers = { 'ACCEPT-LANGUAGE' => 'en' }
-  end
+  # The following only works for poltergeist, which I disabled temp. (unreliable in CI).
+  # config.before(:each, js: true) do
+  #   # feature specs need to use lale in English to find labels etc.
+  #   page.driver.headers = { 'ACCEPT-LANGUAGE' => 'en' }
+  # end
 
   config.after(:each, js: true) do |example|
     if example.exception != nil
