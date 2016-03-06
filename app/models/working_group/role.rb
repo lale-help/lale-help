f93fcd3ace9d1b908a606d2cb34a0daad77d0910
Kristy Gateley <kristysnet@gmail.com>
2016-03-05 21:02:06 -0800

    Organizers list showing non-organizers #183

--- app/models/working_group/role.rb
+++ app/models/working_group/role.rb
@@ -9,6 +9,8 @@ class WorkingGroup::Role < ActiveRecord::Base
 
   ORGANIZER_TYPES  = %w(admin member)
   ORGANIZER_TYPE_IDS  = ORGANIZER_TYPES.map {|id| WorkingGroup::Role.role_types[id] }
+  LEADERSHIP_TYPES = %w(admin)
+  LEADERSHIP_TYPES_IDS  = LEADERSHIP_TYPES.map {|id| WorkingGroup::Role.role_types[id] }
 
   scope :for_circle, ->(circle){ for_group(circle.working_groups) }
   scope :for_group, ->(group){ where(working_group: group ) }
