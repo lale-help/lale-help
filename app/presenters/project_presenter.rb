4270ff6ba55c02e3f84c485f847e175488a23cb6
Phillip Oertel <me@phillipoertel.com>
2016-04-08 16:03:30 +0200

    Fiddle with project start/due dates (project period) #29

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -11,13 +11,19 @@ class ProjectPresenter < Presenter
   end
 
   let(:start_date) do
-    date = _.tasks.minimum("due_date")
-    I18n.l(date, format: :long) if date
+    dates = []
+    dates << _.tasks.minimum("start_date")
+    # quick hack: if no task has a start date, use the earliest end date. 
+    dates << _.tasks.minimum("due_date")
+    dates << _.supplies.minimum("due_date")
+    I18n.l(dates.compact.min, format: :long) unless dates.empty?
   end
 
   let(:due_date) do
-    date = _.tasks.maximum("due_date")
-    I18n.l(date, format: :long) if date
+    dates = []
+    dates << _.tasks.maximum("due_date")
+    dates << _.supplies.maximum("due_date")
+    I18n.l(dates.compact.max, format: :long) unless dates.empty?
   end
 
   let(:has_date_range?) do
@@ -25,7 +31,9 @@ class ProjectPresenter < Presenter
   end
 
   let(:date_range_sentence) do
-    I18n.t('circle.projects.show.date_range_sentence', start_date: start_date, due_date: due_date)
+    if start_date && due_date
+      I18n.t('circle.projects.show.date_range_sentence', start_date: start_date, due_date: due_date)
+    end
   end
 
   let(:tasks) do
