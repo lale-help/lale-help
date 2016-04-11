adea74f8c7bed995ba6af5ad0c664d538a0c396b
Phillip Oertel <me@phillipoertel.com>
2016-04-11 23:08:35 +0200

    Add cancel button for editing task. Also made link_to_xy text configurable, but I didn't need it in the end. #286

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,22 +1,23 @@
 module LinksHelper
 
-  def link_to_user(user)
+  def link_to_user(user, text: user.name)
     href = circle_member_path(user.primary_circle, user)
-    link_to(html_escape(user.name), href)
+    link_to(html_escape(text), href)
   end
   
-  def link_to_working_group(wg)
+  def link_to_working_group(wg, text: wg.name)
     href = circle_working_group_path(wg.circle, wg)
-    link_to(html_escape(wg.name), href)
+    link_to(html_escape(text), href)
   end
 
-  def link_to_circle(circle)
+  def link_to_circle(circle, text: circle.name)
     href = circle_path(circle)
-    link_to(html_escape(circle.name), href)
+    link_to(html_escape(text), href)
   end
 
-  def link_to_project(project)
+  def link_to_project(project, text: project.name)
     href = circle_project_path(project.circle, project)
-    link_to(html_escape(project.name), href)
+    link_to(html_escape(text), href)
   end
+  
 end
