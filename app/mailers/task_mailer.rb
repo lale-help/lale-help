8dc529272517b064d6c1ef713525e7e40e1f035f
Kristy Gateley <kristysnet@gmail.com>
2016-04-20 08:51:21 -0700

    Add generic merge variable to task mailer #289

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,6 +1,6 @@
 class TaskMailer < BaseMandrillMailer
   def task_reminder(task, user, token)
-    build_message(user.language, user.email) do
+    build_message(user.language, user.email, task.organizer.try(:email)) do
       merge_vars(user, task).merge({
         "TASK_UNASSIGN_URL" => handle_token_url(token.code, status: :decline),
         "TASK_CONFIRM_URL"  => handle_token_url(token.code, status: :confirm)
@@ -10,7 +10,7 @@ class TaskMailer < BaseMandrillMailer
 
 
   def task_completion_reminder(task, user, token)
-    build_message(user.language, user.email) do
+    build_message(user.language, user.email, task.organizer.try(:email)) do
       merge_vars(user, task).merge({
         "TASK_MARK_COMPLETE_URL" => handle_token_url(token.code, status: :completed),
         "TASK_ADD_COMMENT_URL"   => handle_token_url(token.code)
@@ -20,7 +20,7 @@ class TaskMailer < BaseMandrillMailer
 
 
   def task_invitation(task, user, token)
-    build_message(user.language, user.email) do
+    build_message(user.language, user.email, task.organizer.try(:email)) do
       merge_vars(user, task).merge({
         "WORKGROUP"        => task.working_group.name,
         "TASK_URL"         => handle_token_url(token.code),
@@ -32,7 +32,7 @@ class TaskMailer < BaseMandrillMailer
 
 
   def task_change(task, user)
-    build_message(user.language, user.email) do
+    build_message(user.language, user.email, task.organizer.try(:email)) do
       merge_vars(user, task).merge({
         "TASK_REVIEW_URL" => handle_token_url(user.login_token.code, redirect: circle_task_path(task.circle, task)),
       })
@@ -40,7 +40,7 @@ class TaskMailer < BaseMandrillMailer
   end
 
   def task_comment(task, comment, user)
-    build_message(user.language, user.email) do
+    build_message(user.language, user.email, task.organizer.try(:email)) do
       merge_vars(user, task).merge({
         "TASK_COMMENT"    => comment.body,
         "TASK_REVIEW_URL" => handle_token_url(user.login_token.code, redirect: circle_task_path(task.circle, task)),
@@ -60,7 +60,10 @@ class TaskMailer < BaseMandrillMailer
       "TASK_DESCRIPTION"      => task.description(length: 100),
       "TASK_DUE_DATE"         => task.scheduling_sentence,
       "TASK_TIME_REQUIRED"    => task.duration_text,
-      "TASK_HELPERS_REQUIRED" => task.volunteer_count_required
+      "TASK_HELPERS_REQUIRED" => task.volunteer_count_required,
+      "TASK_URL_MAIN"         => handle_token_url(user.login_token.code, redirect: circle_task_path(task.circle, task)),
+      "CIRCLE_NAME"           => task.circle.name,
+      "TASK_ORGANIZER"        => task.organizer.try(:name)
     }
   end
 end
