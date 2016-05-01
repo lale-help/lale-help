5a609f3d67e0f4e819f6d725409d79536f06861a
Kristy Gateley <kristysnet@gmail.com>
2016-04-30 21:12:50 -0700

    Refactored seed files to use mutations

--- db/seeds/pull_request/circle.seeds.rb
+++ db/seeds/pull_request/circle.seeds.rb
@@ -2,42 +2,39 @@
 # This file creates an **admin** user.
 # This should never be run on any staging or production environment
 
-location = Location.find_or_create_by(
-    geocode_query: 'Munich',
-    latitude: 48.1351253,
-    longitude: 11.5819806,
-    timezone: 'Europe/Berlin'
-)
-Address.reset_callbacks(:save)
-address = Address.find_or_create_by(city: 'Munich', country: 'DE', location_id: location.id)
-circle = Circle.find_or_create_by(name: "Default", address: address)
-working_group = WorkingGroup.find_or_create_by(name: "Default WG", circle: circle)
-
 
 # Create Admin User
-admin = User.find_or_create_by(first_name: "Lale", last_name: "Admin", is_admin: true, primary_circle_id: circle.id, status: User.statuses[:active])
-if admin.identity.nil?
-  User::Identity.create(user_id: admin.id, email: "admin@lale.help", password: "Example1234")
+admin = User::Identity.find_by(email: 'admin@lale.help').try(:user)
+if admin.nil?
+  admin = User::Create.new(first_name: 'Lale', last_name: 'Admin',
+                           email: 'admin@lale.help', password: 'Example1234',
+                           password_confirmation: 'Example1234',
+                           accept_terms: true, language: User.languages[:de]).submit.result
+  admin.update_attributes(is_admin: true)
 end
-if admin.address.nil?
-  admin.address = Address.create(city: 'Munich', country: 'DE', location_id: location.id)
-  admin.save
+circle = Circle.find_by(name: 'Default')
+if circle.nil?
+  circle = Circle::CreateForm.new(name: 'Default', city: 'Munich', country: 'DE',
+                                  language: 'de', user: admin,
+                                  must_activate_users: false, postal_code: '80333').submit.result
 end
-role = Circle::Role.send('circle.admin')
-role.find_or_create_by(user: admin, circle: circle)
-working_group.roles.admin.find_or_create_by! user: admin
+
+if WorkingGroup.find_by(name: 'Default WG', circle_id: circle.id).nil?
+  working_group = WorkingGroup::Create.new(working_group: WorkingGroup.new(circle_id: circle.id),
+                                           name: 'Default WG', type: :public)
+  working_group.submit
+  WorkingGroup::AddUserForm.new(working_group: working_group.working_group, type: :organizer,
+                                user_id: admin.id).submit
+end
 
 
 # Create some volunteer users
 (1..5).each do |i|
-  user = User.find_or_create_by(first_name: "Lale", last_name: "App#{i}", status: User.statuses[:active])
-  if user.identity.nil?
-    User::Identity.create(user_id: user.id, email: "user#{1}@lale.help", password: "Example1234")
+  if User::Identity.find_by(email: "user#{i}@lale.help").nil?
+    user = User::Create.new(first_name: 'Lale', last_name: "App#{i}",
+                            email: "user#{i}@lale.help", password: 'Example1234',
+                            password_confirmation: 'Example1234',
+                            accept_terms: true, language: User.languages[:de]).submit.result
+    Circle::Join.new(user: user, circle_id: circle.id).submit
   end
-  if user.address.nil?
-    user.address = Address.create(city: 'Munich', country: 'DE', location_id: location.id)
-    user.save
-  end
-  role = Circle::Role.send('circle.volunteer')
-  role.find_or_create_by(user: user, circle: circle)
 end
