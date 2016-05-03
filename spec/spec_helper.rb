09eef1581736cfce51b72c2e9140ad33b3b02292
Phillip Oertel <me@phillipoertel.com>
2016-05-03 14:53:33 +0200

    Require the expect(x).to ... syntax for RSpec (not should)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect
   end
 end
