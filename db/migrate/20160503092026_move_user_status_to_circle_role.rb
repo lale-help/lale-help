4c22c5694792f6f48d35a0d3a4679daf65505fde
Phillip Oertel <me@phillipoertel.com>
2016-05-04 18:59:07 +0200

    Fix migration; status isn't on the user any more.

--- db/migrate/20160503092026_move_user_status_to_circle_role.rb
+++ db/migrate/20160503092026_move_user_status_to_circle_role.rb
@@ -1,9 +1,11 @@
 class MoveUserStatusToCircleRole < ActiveRecord::Migration
 
+  STATUS_MAP = %i(pending active blocked)
+
   def up
     add_column :circle_roles, :status, :integer
     User.find_each do |user|
-      status_id = User.statuses[user.status]
+      status_id = STATUS_MAP.index(user.status)
       user.circle_roles.each { |role| role.update_attribute(:status, status_id) }
     end
     remove_column :users, :status
@@ -13,7 +15,7 @@ class MoveUserStatusToCircleRole < ActiveRecord::Migration
     add_column :users, :status, :integer
     User.find_each do |user|
       status = user.circle_roles.pluck(:status).include?(User.statuses[:pending]) ? :pending : :active
-      status_id = User.statuses[status]
+      status_id = STATUS_MAP.index(status)
       user.update_attribute(:status, status_id)
     end
     remove_column :circle_roles, :status
