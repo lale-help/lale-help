0a5e8f68ac19f73095e704dfca85511b0eefba85
Phillip Oertel <me@phillipoertel.com>
2016-05-11 17:26:06 +0200

    Simplify invitation email even more

--- app/mutations/task/notifications/invitation_email.rb
+++ app/mutations/task/notifications/invitation_email.rb
@@ -6,23 +6,24 @@ class Task::Notifications::InvitationEmail < Mutations::Command
   end
 
   def execute
-    invitees = volunteers.select { |user| user.email.present? }
     invitees.each do |user|
       token = Token.task_invitation.create! context: { user_id: user.id, task_id: task.id }
       TaskMailer.task_invitation(task, user, token).deliver_now
     end
 
-    Task::Comments::Invited.run(task: task, message: 'invited', user: current_user, invite_count: invitees.count)
+    Task::Comments::Invited.run(task: task, user: current_user, invite_count: invitees.count)
 
-    OpenStruct.new(volunteers: volunteers)
+    OpenStruct.new(volunteers: invitees)
   end
 
   private
 
-  def volunteers
-    @volunteers ||= begin
-      volunteers = (type == "circle" ? task.circle.volunteers : task.working_group.users)
-      volunteers.active.to_a.reject { |u| u == current_user || task.volunteers.include?(u) }
+  def invitees
+    @invitees ||= begin
+      invitees = (type == "circle" ? task.circle.volunteers : task.working_group.users)
+      invitees.active.to_a.reject do |u| 
+        u == current_user || task.invitees.include?(u) || !user.email.present?
+      end
     end
   end
 end
