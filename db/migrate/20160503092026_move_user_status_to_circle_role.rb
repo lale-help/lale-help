71b4b2d38be26a96591aebd2e455836e56049f4b
Phillip Oertel <me@phillipoertel.com>
2016-05-12 19:06:02 +0200

    Fix user status migration (sorry …)

--- db/migrate/20160503092026_move_user_status_to_circle_role.rb
+++ db/migrate/20160503092026_move_user_status_to_circle_role.rb
@@ -1,12 +1,21 @@
-class MoveUserStatusToCircleRole < ActiveRecord::Migration
+class User < ActiveRecord::Base
+  enum status: %i(pending active blocked)
+end
 
-  STATUS_MAP = %i(pending active blocked)
+class Circle::Role < ActiveRecord::Base
+  enum status: %i(pending active blocked)
+end
+
+class MoveUserStatusToCircleRole < ActiveRecord::Migration
 
   def up
     add_column :circle_roles, :status, :integer
     User.find_each do |user|
-      status_id = STATUS_MAP.index(user.status)
-      user.circle_roles.each { |role| role.update_attribute(:status, status_id) }
+      if user.status
+        user.circle_roles.each { |role| role.update_attribute(:status, user.status) }
+      else 
+        puts "User #{user.id} has status nil, not updating status on circle roles."
+      end
     end
     remove_column :users, :status
   end
@@ -14,9 +23,14 @@ class MoveUserStatusToCircleRole < ActiveRecord::Migration
   def down
     add_column :users, :status, :integer
     User.find_each do |user|
-      status_id = STATUS_MAP.index(:active) # hard fix
-      user.update_attribute(:status, status_id)
+      if user.circle_roles.present?
+        status = user.circle_roles.present? && user.circle_roles.all?(&:active?) ? :active : :pending
+        user.update_attribute(:status, status)
+      else
+        user.update_attribute(:status, nil)
+      end
     end
     remove_column :circle_roles, :status
   end
+
 end
