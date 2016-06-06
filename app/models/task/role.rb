a7cc17984b263577e07b57c83dd6a1093ca3658a
Phillip Oertel <me@phillipoertel.com>
2016-06-06 18:43:58 +0200

    Revert "Task::Role: consider two roles the same when role, user and task match."
    This reverts commit 73f63270171ac51d070a827d80e775eb6e8e4c57.

--- app/models/task/role.rb
+++ app/models/task/role.rb
@@ -10,12 +10,8 @@ class Task::Role < ActiveRecord::Base
 
   scope :with_role, ->(role_name) { where(role_type: role_types[role_name]) }
 
+
   def name
     self[:name] || I18n.t!("roles.name.#{self.role_type}")
-  end
-
-  # override default comparison method, treating roles as value objects.
-  def ==(other_task)
-    %i(role_type user_id task_id).all? { |attribute| self.send(attribute) == other_task.send(attribute) }
   end
 end
