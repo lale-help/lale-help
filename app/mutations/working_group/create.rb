b8d78cdfb46bb5f943e68649a6091f261959565e
Phillip Oertel <me@phillipoertel.com>
2016-06-06 12:59:17 +0200

    Simplify/wrap conditions

--- app/mutations/working_group/create.rb
+++ app/mutations/working_group/create.rb
@@ -1,9 +1,8 @@
 class WorkingGroup::Create < WorkingGroup::BaseForm
   class Submit < WorkingGroup::BaseForm::Submit
     def validate
-      if WorkingGroup.where('lower(name) = ? AND circle_id = ?', name.downcase, working_group.circle_id).count > 0
-        add_error(:name, :not_unique)
-      end
+      condition = ['lower(name) = ? AND circle_id = ?', name.downcase, working_group.circle_id]
+      add_error(:name, :not_unique) if WorkingGroup.exists?(condition)
     end
   end
 end
