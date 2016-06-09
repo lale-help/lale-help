b2a9769a178881367844d692ed7989f928498976
Phillip Oertel <me@phillipoertel.com>
2016-06-09 16:20:44 +0200

    Create separate vendor file to require external dependencies.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,5 @@
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require js-routes
-//= require handlebars.runtime
-//= require i18n
-//= require i18n/translations
-//= require clipboard/dist/clipboard
-//= require tablesorter/jquery.tablesorter
-//= require datetimepicker/build/jquery.datetimepicker.full.min
-//= require select2/dist/js/select2
-//= require remodal/dist/remodal
+// require all 3rd party stuff in vendor.
+//= require 'vendor'
 
 //= require '_lale'
 
---
+++ app/assets/javascripts/vendor.js
@@ -0,0 +1,14 @@
+// require all 3rd party stuff in vendor.
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require js-routes
+//= require handlebars.runtime
+//= require i18n
+//= require i18n/translations
+//= require clipboard/dist/clipboard
+//= require tablesorter/jquery.tablesorter
+//= require datetimepicker/build/jquery.datetimepicker.full.min
+//= require select2/dist/js/select2
+//= require remodal/dist/remodal
