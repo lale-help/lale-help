e52e88edc303e8331c26965fda57a55d0b070888
Kristy Gateley <kristysnet@gmail.com>
2016-06-11 15:15:32 -0700

    Minor text/styling adjustments per input Removed update email alerts

--- app/mutations/comment/update.rb
+++ app/mutations/comment/update.rb
@@ -1,14 +1,4 @@
 class Comment::Update < Comment::BaseForm
   class Submit < Comment::BaseForm::Submit
-    def execute
-      super.tap do |outcome|
-        if item.is_a? Task
-          (item.users.uniq - [ commenter ]).each do |outboud_user|
-            next unless outboud_user.email.present?
-            TaskMailer.task_comment(item, comment, outboud_user).deliver_now
-          end
-        end
-      end
-    end
   end
 end
