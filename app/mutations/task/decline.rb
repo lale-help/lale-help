243d78cb2d4d11b1d322c0c138270244e697ab81
Phillip Oertel <me@phillipoertel.com>
2016-06-13 17:51:24 +0200

    i18n for unassign

--- app/mutations/task/decline.rb
+++ app/mutations/task/decline.rb
@@ -11,13 +11,13 @@ class Task::Decline < Mutations::Command
 
     assignment.destroy
 
-    (task.users.uniq - [ user ]).each do |outboud_user|
-      next unless outboud_user.email.present?
+    (task.users.uniq - [ user ]).each do |outbound_user|
+      next unless outbound_user.email.present?
       changes = { volunteers: [] } # a slight hack; only the key is relevant
-      TaskMailer.task_change(task, outboud_user, changes).deliver_now
+      TaskMailer.task_change(task, outbound_user, changes).deliver_now
     end
 
-    Task::Comments::Base.run(task: task, message: 'user_unassigned', user: user)
+    Task::Comments::Base.run(task: task, message: 'user_unassigned_self', user: user)
 
     assignment
   end
