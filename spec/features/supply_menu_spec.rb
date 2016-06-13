bb5d4992ae95a660df3d2db9ce406074dc800ae7
Phillip Oertel <me@phillipoertel.com>
2016-06-13 23:40:19 +0200

    Fix spec wording

--- spec/features/supply_menu_spec.rb
+++ spec/features/supply_menu_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-describe 'My test' do
-  context 'uploading a file' do
+describe 'The Add menu' do
+  context 'when user is working group admin' do
 
     let!(:circle) { submit_form(:circle_create_form).result }
     let(:admin) { circle.admin }
     let!(:working_group) { create(:working_group, circle: circle) }
     let!(:role) { create(:working_group_admin_role, user: admin, working_group: working_group) }
 
-    it 'works' do
-      # when thinks aren't working as expected you may want to verify your setup, like this for example:
+    it 'has a link to the supply form' do
+      # when things aren't working as expected you may want to verify your setup, like this for example:
       expect(Circle.count).to eq(1)
       expect(circle.working_groups).to include(working_group)
       # a user needs to be working group admin in order to create tasks & supplies
