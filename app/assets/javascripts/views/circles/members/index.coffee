0da1f148654ea34deaf3115108b9e2aeb28c4120
Kristy Gateley <kristysnet@gmail.com>
2016-06-12 20:40:17 -0700

    Combined organizers and helpers index views

--- app/assets/javascripts/views/circles/members/index.coffee
+++ app/assets/javascripts/views/circles/members/index.coffee
@@ -1,5 +1,6 @@
 ready = ->
   $(".member-list").tablesorter()
+  new Lale.TabNav('.members_list .tab-nav', '.members_list .tab')
 
 $(document).on 'ready', ready
 $(document).on 'page:load', ready
--- app/controllers/circle/members_controller.rb
+++ app/controllers/circle/members_controller.rb
@@ -9,6 +9,7 @@ class Circle::MembersController < ApplicationController
   def index
     authorize! :manage, current_circle
     @members       = active_users
+    @organizers    = organizers
     @total_members = @members.count
   end
 
@@ -54,6 +55,29 @@ class Circle::MembersController < ApplicationController
     current_circle.users.active
       .includes(:identity, :working_groups, :circle_roles)
       .order('last_name asc')
+  end
+
+  def organizers
+    circle_organizers = User.
+        select('users.id, count(*) as role_count').
+        joins(:circle_roles).
+        where(circle_roles: { role_type: Circle::Role::ORGANIZER_TYPE_IDS, circle: current_circle }).
+        group('users.id')
+
+    working_group_organizers = User.
+        select('users.id, count(*) as role_count').
+        joins(:working_group_roles).
+        where(working_group_roles: { role_type: WorkingGroup::Role::ORGANIZER_TYPE_IDS, working_group_id: current_circle.working_groups }).
+        group('users.id')
+
+    counts = (circle_organizers + working_group_organizers).each_with_object(Hash.new) do |user, obj|
+      obj[user.id] ||= 0
+      obj[user.id] += user.role_count
+    end
+
+    all_organizers = User.where(id: counts.keys).includes(:identity, :working_groups, :circle_roles)
+
+    all_organizers.sort_by{|u| -counts[u.id] }
   end
 
 end
