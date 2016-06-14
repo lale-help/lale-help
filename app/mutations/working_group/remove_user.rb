b2f2ac37cf152ffa8c8c164cbb118f5ca3900fb8
Phillip Oertel <me@phillipoertel.com>
2016-06-14 14:49:32 +0200

    Fix working group admin: when a users organizer role was deleted, her member role was deleted as well. This is wrong, the user should stay member. Also adds a spec for WorkingGroup::RemoveUser mutation.

--- app/mutations/working_group/remove_user.rb
+++ app/mutations/working_group/remove_user.rb
@@ -2,21 +2,44 @@ class WorkingGroup::RemoveUser  < Mutations::Command
 
   required do
     model :working_group
+    string :role_type
     integer :user_id
   end
 
+
   def validate
-    add_error(:organizer_id, :at_least_one_required) if user_is_last_organizer? 
+    if user_is_last_admin?
+      add_error(:organizer_id, :at_least_one_required) 
+    end
+    unless %i(member admin).include?(role_type.to_sym)
+      add_error(:role_type, :at_least_one_required) 
+    end
   end
 
   def execute
-    # a user may have an organizer and volunteer role, delete all of them.
-    working_group.roles.where(user_id: user_id).delete_all
+    if (role_type.to_sym == :admin)
+      # only delete admin role, keep the member role
+      role_id = WorkingGroup::Role.role_types[:admin]
+      user_roles.find_by(role_type: role_id).destroy
+    else
+      # can't be admin without being member, delete all roles
+      user_roles.destroy_all
+    end
   end
 
   private
 
-  def user_is_last_organizer?
+  def user_roles
+    User.find(user_id).working_group_roles
+  end
+
+  def find_role
+    user    = User.find(user_id)
+    role_id = WorkingGroup::Role.role_types[role_type]
+    user.working_group_roles.find_by(role_type: role_id)
+  end
+
+  def user_is_last_admin?
     admin_roles = working_group.roles.admin
     admin_roles.count == 1 && admin_roles.first.user_id == user_id
   end
