45a72e043c42e9e36e4276b270d8c1f625f21cc1
Julian Prokay <julian.prokay@workday.com>
2016-07-04 22:13:04 -0700

    updated tests based on comments

--- spec/features/supply_spec.rb
+++ spec/features/supply_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "supply", type: :feature, js: true do
+describe "Supply", type: :feature, js: true do
 
   let(:circle) { create(:circle) }
 
@@ -12,14 +12,14 @@ describe "supply", type: :feature, js: true do
   let(:public_group)  { create(:working_group, is_private: false, circle: circle) }
   let(:private_group) { create(:working_group, is_private: true , circle: circle) }
 
-  def create_supply(name, description, due_date, location)
-    date = I18n.l(due_date) if due_date != nil
-    fill_in "Name of supply",                   with: name
-    fill_in "Description",                      with: description
-    fill_in "Due Date",                         with: date                      
-    fill_in "Location",                         with: location
+  def create_supply(supply)
+    fill_in "Name of supply",                   with: supply.name
+    fill_in "Description",                      with: supply.description
+    fill_in "Due Date",                         with: supply.due_date                      
+    fill_in "Location",                         with: supply.location
     
     click_button  "Create Supply"
+    supply
   end
 
   def visit_add_supply(user)
@@ -39,7 +39,7 @@ describe "supply", type: :feature, js: true do
   end
 
   context 'add supply button' do
-    it 'visible to Admin' do
+    it 'is visible to Admin' do
       visit(circle_path(circle, as: user_1))
       
       expect(find('.button-super', text: /Add/)).to_not eq(nil)
@@ -54,48 +54,36 @@ describe "supply", type: :feature, js: true do
   end
 
   context 'an admin' do
-
     it "adds a supply" do
-      name        = "Test Supply"
-      description = "Test Description"
-      due_date    = Date.today
-      location    = "Oakland, CA 94607, USA"
-
       visit_add_supply(user_1)
-      create_supply(name, description, due_date, location)
+      s = create_supply(FactoryGirl.create(:supply))
 
       expect(page).to have_content("Supply was successfully created.")
-      expect(page).to have_content(name)
-      expect(page).to have_content(description)
-      puts "Formatted: #{due_date.strftime("%A %-d %B %Y")}"
-      expect(page).to have_content("Due Date #{due_date.strftime("%A %-d %B %Y")}")
-      expect(page).to have_content(location)
+      expect(page).to have_content(s.name)
+      expect(page).to have_content(s.description)
+
+      expect(page).to have_content("Due Date #{s.due_date.strftime("%A %-d %B %Y")}")
+      expect(page).to have_content(s.location)
     end
 
     it "edits a supply" do
-      name        = "Test Supply"
-      description = "Test Description"
-      due_date    = Date.today
-      location    = "Oakland, CA 94607, USA"
-
       visit_add_supply(user_1)
-      create_supply(name, description, due_date, location)
+      s = create_supply(FactoryGirl.create(:supply))
       find('.button-super', text: /Edit Supply/).click
       click_on("Edit Supply")
-      fill_in "Name of supply", with: "Edited #{name}"
+      fill_in "Name of supply", with: "Edited #{s.name}"
   
       click_on  "Update Supply"
       expect(page).to have_content("Supply was successfully updated.")
-      expect(page).to have_content("Edited #{name}")
+      expect(page).to have_content("Edited #{s.name}")
     end
   end
 
   context 'add supply page' do
-
     it 'requires a Name, Description, Due Date, and Location' do
       visit_add_supply(user_1)
-
-      create_supply(nil, nil, nil, nil)
+      fill_in "Due Date", with: nil
+      click_button  "Create Supply"
 
       expect(page).to have_content(t("errors.name.empty"))
       expect(page).to have_content(t("errors.due_date.empty"))
