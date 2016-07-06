77162e3d6d2867cda2cf955db8af175bfac7b2b7
Phillip Oertel <me@phillipoertel.com>
2016-07-06 16:38:25 +0200

    Basic HTML structure for sponsorships, incl. some dummy code for images

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,7 +1,14 @@
+require 'faker'
+
 class Sponsor < ActiveRecord::Base
   
   validates :name, presence: true
   validates :url, url: { allow_blank: true }
 
   has_many :sponsorships
+
+  # temp placeholder for the view
+  def logo_url
+    Faker::Placeholdit.image("190x190", 'jpg', 'ffffff', '000', name)
+  end
 end
