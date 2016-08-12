5769662ba643c24b35ed8ea57e7aac6d0c9cbef1
Phillip Oertel <me@phillipoertel.com>
2016-08-13 00:24:17 +0200

    Feature spec for volunteering and declining from a task.

---
+++ spec/features/tasks/volunteer_and_decline_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+describe "Task volunteer and decline spec", type: :feature, js: true do
+
+  let(:circle)    { create(:circle, :with_admin) }
+  let(:admin)     { circle.admin }
+
+  let(:working_group) { create(:working_group, circle: circle, member: admin) }
+  let!(:task) { create(:task, working_group: working_group) }
+
+  let(:task_page) { SPTaskPage.new }
+
+  # FIXME move this spec to a different file
+  describe "navigating to task page" do
+    context "when on the circle dashboard" do
+
+      let(:circle_dashboard) { SPCircleDashboard.new }
+
+      before { circle_dashboard.load(circle_id: circle.id, as_id: admin.id) }
+
+      it "works" do
+        circle_dashboard.tasks.first.click
+        expect(task_page.page_title).to eq(task.name)
+        expect(task_page.description.text).to eq(task.description)
+      end
+
+    end
+  end
+
+  describe "volunteering for a task" do
+    context "when on the task page" do
+      before { task_page.load_for(task, as: admin) }
+
+      it "works" do
+        task_page.volunteer_button.click
+        expect(task_page).to have_decline_button
+        expect(task_page).not_to have_volunteer_button
+        expect(task_page.helper_names).to include(admin.name)
+      end
+    end
+  end
+
+  describe "declining from a task" do
+    context "when on the task page" do
+
+      before { task_page.load_for(task, as: admin) }
+
+      context "when already volunteered" do
+
+        before { task_page.volunteer_button.click }
+
+        it "works" do
+          task_page.decline_button.click
+          expect(task_page).to have_volunteer_button
+          expect(task_page).not_to have_decline_button
+          expect(task_page.helper_names).not_to include(admin.name)
+        end
+      end
+
+    end
+  end
+
+end
---
+++ spec/page_objects/task_page.rb
@@ -0,0 +1,23 @@
+class SPTaskPage < SitePrism::Page
+
+  set_url '/circles/{circle_id}/tasks/{task_id}?as={as_id}'
+
+  element :h1_title, '.task-header .title'
+  element :description, '.task-header .description'
+  element :volunteer_button, '.button-primary', text: "I'll help"
+  element :decline_button, '.button-primary', text: "I can't help anymore"
+  elements :helper_badges, '.users-box .user-badge'
+
+  def page_title
+    h1_title.text
+  end
+
+  def load_for(task, as:)
+    load(circle_id: task.circle.id, task_id: task.id, as_id: as.id)
+  end
+
+  def helper_names
+    helper_badges.map { |badge| badge.find('.user-name-shortened').text }
+  end
+
+end
