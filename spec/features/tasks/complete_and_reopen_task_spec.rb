556c449070909c031a37455b61bc2238922e9318
Phillip Oertel <me@phillipoertel.com>
2016-08-17 15:42:36 +0200

    added feature spec: complete and reopen task.

---
+++ spec/features/tasks/complete_and_reopen_task_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe "Task volunteer and decline spec", js: true do
+
+  let(:circle)        { create(:circle, :with_admin_and_working_group) }
+  let(:admin)         { circle.admin }
+  let(:working_group) { circle.working_groups.first }
+
+  let!(:task) { create(:task, working_group: working_group) }
+
+  let(:task_page) { PageObject::Task::Page.new }
+
+  before { task_page.load_for(task, as: admin) }
+
+  describe "completing a task" do
+    it "works" do
+      task_page.edit_button.click
+      task_page.complete_button.click
+      expect(task_page).to be_completed
+    end
+  end
+
+  describe "reopening a task" do
+
+    context "when task is completed" do
+
+      before do
+        task_page.edit_button.click
+        task_page.complete_button.click
+      end
+
+      it "works" do
+        expect(task_page).to be_completed
+        task_page.edit_button.click
+        task_page.reopen_button.click
+        expect(task_page).to be_new
+      end
+    end
+
+  end
+
+end
--- spec/page_objects/task/page.rb
+++ spec/page_objects/task/page.rb
@@ -12,6 +12,10 @@ module PageObject
       element :volunteer_button, '.button-primary', text: "I'll help"
       element :decline_button, '.button-primary', text: "I can't help anymore"
       
+      element :edit_button, '.task-edit-menu .button-super'
+      element :complete_button, '.task-edit-menu .menu a', text: 'Complete Task'
+      element :reopen_button, '.task-edit-menu .menu a', text: 'Reopen Task'
+
       element :task_badge, '.users-box .task-badge'
       elements :helper_badges, '.users-box .user-badge'
 
@@ -26,6 +30,14 @@ module PageObject
 
       def helper_names
         helper_badges.map { |badge| badge.find('.user-name-shortened').text }
+      end
+
+      def completed?
+        find('.task.urgency--complete')
+      end
+
+      def new?
+        find('.task.urgency--new')
       end
 
     end
