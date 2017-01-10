a82f39d20124b1e6f8eecb89dd4b3d45fe156149
Phillip Oertel <me@phillipoertel.com>
2017-01-10 11:29:10 +0100

    Move controller method with business logic to mutation #443 #440

--- app/mutations/project/complete.rb
+++ app/mutations/project/complete.rb
@@ -2,9 +2,16 @@ class Project::Complete < Mutations::Command
 
   required do
     model :project
+    model :current_user, class: User
   end
 
   def execute
     project.complete!
+    close_all_tasks_supplies
+  end
+
+  def close_all_tasks_supplies
+    project.tasks.incomplete.each { |t| Task::Complete.run(user: current_user, task: t) }
+    project.supplies.incomplete.each { |s| Supply::Complete.run(user: current_user, supply: s) }
   end
 end
