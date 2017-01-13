3064a6ff2209ced541bd420fb9e26b5b0cb94d83
Phillip Oertel <me@phillipoertel.com>
2017-01-13 11:26:03 +0100

    Use refile's filesystem backend rather than s3 in test env.

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,18 +1,18 @@
-require "refile/s3"
+if Rails.env.test?
 
-aws_config = {
-  access_key_id: ENV['AWS_ACCESS_KEY'],
-  secret_access_key: ENV['AWS_SECRET_KEY'],
-  region: ENV['AWS_REGION'],
-  bucket: ENV['AWS_BUCKET'],
-}
+  Refile.cache = Refile::Backend::FileSystem.new(Rails.root.join('tmp', 'refile', Rails.env, 'cache'))
+  Refile.store = Refile::Backend::FileSystem.new(Rails.root.join('tmp', 'refile', Rails.env, 'store'))
 
-Refile.cache = Refile::S3.new(prefix: "cache", **aws_config)
-Refile.store = Refile::S3.new(prefix: "store", **aws_config)
+else
 
-# Refile.cdn_host = AppConfig.asset_host
-#
-# if Rails.env.test?
-#   Refile.cache = Refile::Backend::FileSystem.new(Rails.root.join('tmp', 'refile', Rails.env, 'cache'))
-#   Refile.store = Refile::Backend::FileSystem.new(Rails.root.join('tmp', 'refile', Rails.env, 'store'))
-# end
+  require "refile/s3"
+  aws_config = {
+    access_key_id: ENV['AWS_ACCESS_KEY'],
+    secret_access_key: ENV['AWS_SECRET_KEY'],
+    region: ENV['AWS_REGION'],
+    bucket: ENV['AWS_BUCKET'],
+  }
+  Refile.cache = Refile::S3.new(prefix: "cache", **aws_config)
+  Refile.store = Refile::S3.new(prefix: "store", **aws_config)
+  
+end
