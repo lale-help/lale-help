86e21746ffd5e4fcf920f0223bf9046aca11a621
Phillip Oertel <me@phillipoertel.com>
2017-01-13 14:27:48 +0100

    Add feature specs for adding and removing a profile image to the user.

--- spec/page_objects/member/form.rb
+++ spec/page_objects/member/form.rb
@@ -7,6 +7,7 @@ module PageObject
       element :first_name, '#user_first_name'
       element :last_name, '#user_last_name'
       element :profile_image, '#user_profile_image'
+      element :remove_profile_image, '#user_remove_profile_image'
 
       include PageObject::IsForm
 
@@ -20,6 +21,11 @@ module PageObject
         first_name.set(attributes[:first_name]) if attributes[:first_name]
         last_name.set(attributes[:last_name]) if attributes[:last_name]
         profile_image.set(attributes[:profile_image]) if attributes[:profile_image]
+        if attributes[:remove_profile_image]
+          # the form is long, and we need to get at the checkbox after the fold
+          page.current_window.resize_to(1024, 1024)
+          remove_profile_image.set(attributes[:remove_profile_image])
+        end
       end
 
       def submit_button
