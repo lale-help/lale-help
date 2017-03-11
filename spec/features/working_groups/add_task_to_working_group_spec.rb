aa5729f560074a781245d8de75b937c5d8dd88d0
Julian Prokay <julian.prokay@workday.com>
2017-03-11 11:59:42 -0800

    updated spec files and added tests for buttons

---
+++ spec/features/working_groups/add_task_to_working_group_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Add Task to a Working Group", js: true do
+
+  let(:circle)        { create(:circle, :with_admin) }
+  let(:admin)         { circle.admin }
+  let(:working_group) { create(:working_group, :with_members, circle: circle, admin: admin) }
+
+  let(:dashboard_page) { PageObject::WorkingGroup::Dashboard.new }
+  let(:task_form)      { PageObject::Task::Form.new }
+  let(:inputs)         { attributes_for(:task) }
+
+  before { dashboard_page.load(circle_id: circle.id, wg_id: working_group.id, as: admin.id) }
+
+  it "by clicking a button" do
+    dashboard_page.add_task_button.click
+    task_form.submit_with(inputs)
+
+    dashboard_page.load(circle_id: circle.id, wg_id: working_group.id, as: admin.id)
+    dashboard_page.when_loaded do
+      dashboard_page.tab_nav.tasks.click
+      expect(dashboard_page.tasks.count).to eq 1
+    end
+  end
+end
--- spec/page_objects/working_group/dashboard.rb
+++ spec/page_objects/working_group/dashboard.rb
@@ -18,15 +18,18 @@ module PageObject
 
       section :task_list, PageObject::Component::RichItemsList, ".open_tasks"
       delegate :has_task?, :tasks, to: :task_list
+      element :add_task_button, 'a.button-secondary-inverted', text: 'Add Task'
 
       section :supplies_list, PageObject::Component::RichItemsList, ".open_supplies"
       delegate :has_supply?, :supplies, to: :supplies_list
+      element :add_supply_button, 'a.button-secondary-inverted', text: 'Add Supply'
 
       section :projects_list, PageObject::Component::RichItemsList, ".tab.projects .open_projects"
       delegate :has_project?, :projects, to: :projects_list
 
       section :files_list, PageObject::Component::OtherItemsList, ".tab.documents tbody"
       delegate :has_file?, :files, to: :files_list
+      element :add_document_button, 'a.button-primary', text: 'New Document'
 
       section :header, PageObject::Component::CollectionDashboardHeader, '.collection-dashboard .header'
       delegate :headline, :description, to: :header
