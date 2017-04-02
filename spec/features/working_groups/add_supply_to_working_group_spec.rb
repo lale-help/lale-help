bf2afb2209c40cdda27353a47647c9a740bb324b
Christian Meier <meier.kristian@gmail.com>
2017-04-02 11:36:12 +0200

    workaround flaky tests

--- spec/features/working_groups/add_supply_to_working_group_spec.rb
+++ spec/features/working_groups/add_supply_to_working_group_spec.rb
@@ -6,9 +6,12 @@ describe "Add Supply to a Working Group", js: true do
   let(:admin)         { circle.admin }
   let(:working_group) { create(:working_group, :with_members, circle: circle, admin: admin) }
 
-  let(:dashboard_page) { PageObject::WorkingGroup::Dashboard.new }
-  let(:supply_form)    { PageObject::Supply::Form.new }
-  let(:inputs)         { attributes_for(:supply) }
+  let(:dashboard_page)  { PageObject::WorkingGroup::Dashboard.new }
+  # when reusing the regular page for some assertions I got intermittent errors;
+  # not sure why. Maybe the page didn't "realize" it got reloaded.
+  let(:dashboard_page2) { PageObject::WorkingGroup::Dashboard.new }
+  let(:supply_form)     { PageObject::Supply::Form.new }
+  let(:inputs)          { attributes_for(:supply) }
 
   before { dashboard_page.load(circle_id: circle.id, wg_id: working_group.id, as: admin.id) }
   before { dashboard_page.tab_nav.supplies.click }
@@ -17,10 +20,10 @@ describe "Add Supply to a Working Group", js: true do
     dashboard_page.add_supply_button.click
     supply_form.submit_with(inputs)
 
-    dashboard_page.load(circle_id: circle.id, wg_id: working_group.id, as: admin.id)
-    dashboard_page.when_loaded do
-      dashboard_page.tab_nav.supplies.click
-      expect(dashboard_page.supplies.count).to eq 1
+    dashboard_page2.load(circle_id: circle.id, wg_id: working_group.id, as: admin.id)
+    dashboard_page2.when_loaded do
+      dashboard_page2.tab_nav.supplies.click
+      expect(dashboard_page2.supplies.count).to eq 1
     end
   end
 end
--- spec/features/working_groups/add_task_to_working_group_spec.rb
+++ spec/features/working_groups/add_task_to_working_group_spec.rb
@@ -6,9 +6,12 @@ describe "Add Task to a Working Group", js: true do
   let(:admin)         { circle.admin }
   let(:working_group) { create(:working_group, :with_members, circle: circle, admin: admin) }
 
-  let(:dashboard_page) { PageObject::WorkingGroup::Dashboard.new }
-  let(:task_form)      { PageObject::Task::Form.new }
-  let(:inputs)         { attributes_for(:task) }
+  let(:dashboard_page)  { PageObject::WorkingGroup::Dashboard.new }
+  # when reusing the regular page for some assertions I got intermittent errors;
+  # not sure why. Maybe the page didn't "realize" it got reloaded.
+  let(:dashboard_page2) { PageObject::WorkingGroup::Dashboard.new }
+  let(:task_form)       { PageObject::Task::Form.new }
+  let(:inputs)          { attributes_for(:task) }
 
   before { dashboard_page.load(circle_id: circle.id, wg_id: working_group.id, as: admin.id) }
 
@@ -16,10 +19,10 @@ describe "Add Task to a Working Group", js: true do
     dashboard_page.add_task_button.click
     task_form.submit_with(inputs)
 
-    dashboard_page.load(circle_id: circle.id, wg_id: working_group.id, as: admin.id)
-    dashboard_page.when_loaded do
-      dashboard_page.tab_nav.tasks.click
-      expect(dashboard_page.tasks.count).to eq 1
+    dashboard_page2.load(circle_id: circle.id, wg_id: working_group.id, as: admin.id)
+    dashboard_page2.when_loaded do
+      dashboard_page2.tab_nav.tasks.click
+      expect(dashboard_page2.tasks.count).to eq 1
     end
   end
 end
